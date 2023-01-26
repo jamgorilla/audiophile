635011cba77ad9687a974e95fe057a5196e71173
jamgorilla <jamesmurphy507@hotmail.com>
2023-01-26 16:55:56 +0000

    footer complete

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,10 +1,75 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '../../public/assets/shared/desktop/logo.svg';
+import facebook from '../../public/assets/shared/desktop/icon-facebook.svg';
+import twitter from '../../public/assets/shared/desktop/icon-twitter.svg';
+import instagram from '../../public/assets/shared/desktop/icon-instagram.svg';
+
+import styles from '../../styles/Footer.module.scss';
+
+import navListData from '../../navListData.json';
 
 function Footer() {
-    return <footer>
-        <h1>Footer</h1>
+  return (
+    <footer className={styles.main_footer}>
+      <div className={styles.logo_container}>
+        <Image
+          src={logo}
+          height={'58px'}
+          width={'58px'}
+          alt="logo"
+          className="logo"
+        />
+      </div>
+
+      <p className={styles.description_p}>
+        Audiophile is an all in one stop to fulfill your audio needs. We're a
+        small team of music lovers and sound specialists who are devoted to
+        helping you get the most out of personal audio. Come and visit our demo
+        facility - we’re open 7 days a week.
+      </p>
+      <p className={styles.copyright_p}>Copyright 2023. All Rights Reserved</p>
+
+      <div className={styles.explicit_nav_list}>
+        <li key="0">
+          <h6>
+            <Link href="/">Home</Link>
+          </h6>
+        </li>
+
+        {navListData.map((element) => (
+          <li key={element.id}>
+            <h6>
+              <Link href={element.pageLink}>{element.title}</Link>
+            </h6>
+          </li>
+        ))}
+      </div>
+      <div className={styles.social_media_icons}>
+        <Image
+          src={facebook}
+          height={'58px'}
+          width={'58px'}
+          alt="logo"
+          className="logo"
+        />
+        <Image
+          src={twitter}
+          height={'58px'}
+          width={'58px'}
+          alt="logo"
+          className="logo"
+        />
+        <Image
+          src={instagram}
+          height={'58px'}
+          width={'58px'}
+          alt="logo"
+          className="logo"
+        />
+      </div>
     </footer>
+  );
 }
 
 export default Footer;
--- components/mainNavigation/MainNavigation.js
+++ components/mainNavigation/MainNavigation.js
@@ -1,78 +1,82 @@
 // import CustomForwardImage from './CustomForwardImage.js';
-import Image from 'next/image'
-import cart from '../../public/assets/shared/desktop/icon-cart.svg'
-import logo from '../../public/assets/shared/desktop/logo.svg'
-import hamburger from '../../public/assets/shared/tablet/icon-hamburger.svg'
-import styles from '../../styles/MainNavigation.module.scss'
-import Link from 'next/link'
-import NavList from './NavList'
+import Image from 'next/image';
+import cart from '../../public/assets/shared/desktop/icon-cart.svg';
+import logo from '../../public/assets/shared/desktop/logo.svg';
+import hamburger from '../../public/assets/shared/tablet/icon-hamburger.svg';
+import styles from '../../styles/MainNavigation.module.scss';
+import Link from 'next/link';
+import NavList from './NavList';
 
-import navListData from '../../navListData.json'
+import navListData from '../../navListData.json';
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 function MainNavigation(props) {
+  const [open, setOpen] = useState(false);
 
+  useEffect(() => {
+    if (open === true) {
+      //add open class to body
+      document.body.classList.add('open');
+    } else {
+      //remove open class from body
+      document.body.classList.remove('open');
+    }
+  }, [open]);
 
-    const [open, setOpen] = useState(false);
+  return (
+    <nav className={styles.main_navigation}>
+      <div
+        className={`${styles.hamburger_container} btn1`}
+        onClick={() => setOpen(!open)}
+      >
+        <Image src={hamburger} alt="hamburger-menu" />
 
-    useEffect(() => {
-        if (open === true) {
-            //add open class to body
-            document.body.classList.add( 'open' );
-        } else {
-            //remove open class from body
-            document.body.classList.remove( 'open' );
-        }
-    }, [open])
-    
-    return <nav className={ styles.main_navigation }>
-            <div 
-                className={ `${styles.hamburger_container} btn1` }
-                onClick={ () => setOpen(!open) }>
-                    <Image 
-                        src={hamburger}
-                        alt="hamburger-menu"
-                    />
+        {open && (
+          <div className={styles.fixed_nav_container}>
+            <NavList navigationArray={navListData} />
+          </div>
+        )}
+      </div>
 
-                {open && <div className={ styles.fixed_nav_container } ><NavList navigationArray={ navListData }/></div> }
+      <div className={styles.explicit_nav_list}>
+        <li key="0">
+          <h5>
+            <Link href="/">Home</Link>
+          </h5>
+        </li>
 
-            </div>
+        {navListData.map((element) => (
+          <li key={element.id}>
+            <h5>
+              <Link href={element.pageLink}>{element.title}</Link>
+            </h5>
+          </li>
+        ))}
+      </div>
 
-            <div className={ styles.explicit_nav_list }>
-                <li key="0"><h5><Link href="/">Home</Link></h5></li>
-                
-               { navListData.map((element) => (
-                    <li key={element.id}><h5><Link href={ element.pageLink }>{element.title}</Link></h5></li>
-                ))}
-            </div>
-
-            <div className={ styles.logo_container }>
-                <Image 
-                src={ logo }
-                //layout='responsive'
-                height={'58px'}
-                width={'58px'}
-                alt="logo"
-                className='logo'
-                />
-            </div>
-            <div className={ styles.cart_container }>
-            <Image 
-                src={ cart }
-                //layout='responsive'
-                height={'58px'}
-                width={'58px'}
-                alt="cart"
-                className='cart'
-                />
-            </div>
-
-
+      <div className={styles.logo_container}>
+        <Image
+          src={logo}
+          //layout='responsive'
+          height={'58px'}
+          width={'58px'}
+          alt="logo"
+          className="logo"
+        />
+      </div>
+      <div className={styles.cart_container}>
+        <Image
+          src={cart}
+          //layout='responsive'
+          height={'58px'}
+          width={'58px'}
+          alt="cart"
+          className="cart"
+        />
+      </div>
     </nav>
+  );
 }
-
-
-
 
 export default MainNavigation;
