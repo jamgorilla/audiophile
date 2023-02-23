909405fe858553fe8de67b9177438d1fc17f7799
jamgorilla <jamesmurphy507@hotmail.com>
2023-02-23 14:25:25 +0000

    added meta viewport device width to head

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,21 @@
 // pages/_document.js
 
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  
   return (
     <Html>
       <Head>
         <link rel="icon" href="/assets/music-note-icon.png" type="image/png" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
