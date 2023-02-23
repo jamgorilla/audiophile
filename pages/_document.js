316c96755d89b57586bb1db56b5820a8d0e2620f
jamgorilla <jamesmurphy507@hotmail.com>
2023-02-23 14:35:51 +0000

    changed initial viewport zoom to 1.5

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
         <link rel="icon" href="/assets/music-note-icon.png" type="image/png" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0"
+          content="width=device-width, initial-scale=1.5"
         ></meta>
       </Head>
       <body>
